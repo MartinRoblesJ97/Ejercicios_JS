ee250ed889296782a07b96689cab102d589c1c0d
MartinR <martin.alejandro.robles.juarez@gmail.com>
2022-08-01 19:36:48 -0500

    Se anexan los documentos Script.js, Variables.js e Index.html

---
+++ assets/JS/Script.js
@@ -0,0 +1,23 @@
+/* Esta es una alerta para saber que el HTML y el JS están enlazados*/
+//También se puede comentar una sola línea así
+alert ("Conexión exitosa");
+
+//Programa para calcular el área de un triangulo
+base = prompt("Ingrese la base del triangulo");
+altura = prompt("Ingrese la altura del triangulo");
+areaTriangulo = (base*altura)/2;
+
+alert("areaTriangulo");
+
+//Concatenación de mensaje + resultado
+console.log("El área del triangulo es:" + areaTriangulo);l area del triangulo es: " + areaTriangulo); 
+
+
+//Una alerta es una ventana de dialogo que muestra un mensaje, solo muestra informacion.
+
+
+//Pedimos la informacion al usuario con un prompt, la guardamos en una variable llamada nombre para despues mostrarla
+
+
+//Usamos un console.log para mostrar lo que se guardo en la variable nombre
+
---
+++ assets/JS/variables.js
@@ -0,0 +1,37 @@
+/*Palabras reservadas; no utilizar para declarar una variable, por ejemplo;
+If, return, true, false, for, while, let, var*/
+
+var a;
+var a1;
+var a_Nombre;
+
+//Declaración de variables
+
+//Type number
+var edad = 24;
+var peso = 78;
+var miopia = 3;
+var astigmatismo = 3;
+
+//Type Boolean
+var ciego = true;
+var hambre = true;
+var bateria = false;
+var ia = true;
+
+//Type String
+var nombre = "Martín Robles"
+var dir = "Guadalajara"
+var nacionalidad = "Mexicano"
+
+//Type null
+var ganasDeVivir = null;
+var dineros = null;
+var briAmonestaciones = null;
+
+//Variables declaradas con un mismo valor
+var x, y, z = 4;
+//Variables undefined
+var xx, yy, zz;
+
+var uno = dos = tres = 4;
